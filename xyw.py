c9e03ccc084531b70552d45d6e72ec112c78a88e
NEKOSAIKO <50594518+NEKOSAIKO@users.noreply.github.com>
2023-02-15 14:33:53 +0800

    Update xyw.py

--- xyw.py
+++ xyw.py
@@ -18,39 +18,11 @@ class Login:
         url="http://211.138.135.94/a70.htm"
         #消息头
         headers={
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-            'Cache-Control': 'max-age=0',
-            'Content-Length': '145',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'program=lymx; vlan=0; getUserName=2021509; ISP_select=3; md5_login2=2021509%7C2021509; save_DDDDD=2021509; save_upass=2021509; ip=172.16.26.65; userIp=172.16.26.65; PHPSESSID=2pk01rqsm37i6tbeujpjnb3l20',
-            'Host': '211.138.135.94',
-            'Origin': 'http://211.138.135.94',
-            'Proxy-Connection': 'keep-alive',
-            'Referer': 'http://211.138.135.94/a70.htm',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31',
+           
         }
         #提交的信息
         payload={
-            'DDDDD': '2021509',
-            'upass': '2021509',
-            'R1': '0',
-            'R2': '',
-            'R3': '3',
-            'R6': '0',
-            'para': '00',
-            '0MKKey': '123456',
-            'bttonClicked': '',
-            'redirect_url': '',
-            'err_flag': '',
-            'username': '',
-            'password': '',
-            'user': '',
-            'cmd': '',
-            'Login': ''
-
+       
         }
         try:
             requests.post(url,headers=headers,data=payload)
